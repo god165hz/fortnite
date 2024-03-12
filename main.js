6b4e4c1589cb5f05b1106bf0e5b6eecb11f42adc
god165hz <162719721+god165hz@users.noreply.github.com>
2024-03-12 11:24:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://cdn.sortiraparis.com/images/1001/66131/980059-fortnite-x-stranger-things-eleven-hopper-demogorgon-comment-obtenir-les-skins.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVzNoLrd1v2Q3H0qZCP-2hBpiQL08cm4jI0T9KIGE9tr7wIvFfjjYOuqeKbWKURwLEM5w&usqp=CAU";
+}
+
